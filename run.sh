a1cccc9d22347f8c9f3b0ea3c99c17b6734410ef
sriv <srikanth.ddit@gmail.com>
2018-02-09 12:23:33 +0530

    fix package, install scripts

--- run.sh
+++ run.sh
@@ -28,22 +28,28 @@ function package() {
     cp src/launcher.sh deploy
     cp src/launcher.cmd deploy
     cp src/dotnet.json deploy
-
-    zip -r ./artifacts/gauge-dotnet-${$(version)}.zip ./deploy
+    mkdir -p artifacts
+    (export version=$(version) && cd deploy && zip -r ../artifacts/gauge-dotnet-$version.zip .)
 }
 
 function install() {
     package
-    gauge install dotnet -f ./artifacts/gauge-dotnet-${$(version)}.zip
+    gauge install dotnet -f ./artifacts/gauge-dotnet-$(version).zip
 }
 
 function uninstall() {
-    gauge uninstall dotnet -v ${$(version)}
+    gauge uninstall dotnet -v $(version)
+}
+
+function forceinstall() {
+    uninstall
+    install
 }
 
 tasks=(build test package install uninstall forceinstall)
 if [[ " ${tasks[@]} " =~ " $1 " ]]; then
     $1
+    exit 0
 fi
 
 echo Options: [build \| test \| package \| install \| uninstall \| forceinstall]
