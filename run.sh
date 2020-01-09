7b8fe5233e4ff11cf45a258bfa2433563e44e0ca
sriv <srikanth.ddit@gmail.com>
2020-01-09 15:33:07 +0530

    fix run script

--- run.sh
+++ run.sh
@@ -24,7 +24,8 @@ function package() {
     checkCommand "dotnet"
     checkCommand "zip"
     rm -rf deploy artifacts
-    dotnet publish -c release -o ./deploy/bin src/Gauge.Dotnet.csproj
+    mkdir -p deploy
+    dotnet publish -c release -o ../deploy/bin src/Gauge.Dotnet.csproj
     cp src/launcher.sh deploy
     cp src/launcher.cmd deploy
     cp src/dotnet.json deploy
