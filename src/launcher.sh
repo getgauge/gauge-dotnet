75ca7abba394f6e311e4eaaf3368138b42a99a35
sriv <srikanth.ddit@gmail.com>
2021-05-05 16:41:29 +0530

    fix launcher.sh
    Signed-off-by: sriv <srikanth.ddit@gmail.com>

--- src/launcher.sh
+++ src/launcher.sh
@@ -1,10 +1,6 @@
-#! /bin/sh
+#!/bin/sh
 
-function checkCommand() {
-    command -v $1 >/dev/null 2>&1 || { echo >&2 "$1 is not installed, aborting."; exit 1; }
-}
-
-checkCommand "dotnet"
+command -v $dotnet >/dev/null 2>&1 || { echo >&2 "dotnet is not installed, aborting."; exit 1; }
 
 if [[ "$(dotnet --version)" == *"3."* ]]; then
     dotnet bin/netcoreapp3.0/Gauge.Dotnet.dll $@
