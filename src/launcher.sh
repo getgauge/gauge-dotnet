65885dfd6770caf64234a384d94f986b34615075
Srikanth <srikanth.ddit@gmail.com>
2021-05-06 09:13:30 +0530

    relative path as arg to dotnet
    Signed-off-by: sriv <srikanth.ddit@gmail.com>

--- src/launcher.sh
+++ src/launcher.sh
@@ -4,8 +4,8 @@ command -v $dotnet >/dev/null 2>&1 || { echo >&2 "dotnet is not installed, abort
 
 if [ "$(dotnet --version)" = *"3."* ]; then
     echo "3.0"
-    dotnet bin/netcoreapp3.0/Gauge.Dotnet.dll $@
+    dotnet ./bin/netcoreapp3.0/Gauge.Dotnet.dll $@
 else
-    dotnet bin/net5.0/Gauge.Dotnet.dll $@
+    dotnet ./bin/net5.0/Gauge.Dotnet.dll $@
 fi
 
