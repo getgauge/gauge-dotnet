728836544bca38dadf49bf275a5423e000f40f07
Srikanth <srikanth.ddit@gmail.com>
2021-05-06 17:17:28 +0530

    Add support for .Net 5.0 #151
    Signed-off-by: sriv <srikanth.ddit@gmail.com>

--- dotnet_publish.ps1
+++
@@ -1,7 +0,0 @@
-$version = $(dotnet --version)
-if ($version.Contains("3")) {
-    dotnet publish -c release -o .\deploy\bin src\Gauge.Dotnet.csproj
-}
-else {
-    dotnet publish -c release -o ..\deploy\bin src\Gauge.Dotnet.csproj
-}
--- src/launcher.sh
+++ src/launcher.sh
@@ -1,3 +1,8 @@
-#! /bin/sh
+#!/bin/sh
 
-dotnet bin/Gauge.Dotnet.dll $@
+command -v $dotnet >/dev/null 2>&1 || { echo >&2 "dotnet is not installed, aborting."; exit 1; }
+
+case "$(dotnet --version)" in
+  3.*) dotnet bin/netcoreapp3.0/Gauge.Dotnet.dll $@ ;;
+  *) dotnet bin/net5.0/Gauge.Dotnet.dll $@ ;;
+esac
