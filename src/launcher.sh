d385ec5ad3283e2603f65dcc9b137831bff8601f
sriv <srikanth.ddit@gmail.com>
2021-05-06 08:53:27 +0530

    fix launcher.sh test condition
    Signed-off-by: sriv <srikanth.ddit@gmail.com>

--- src/launcher.sh
+++ src/launcher.sh
@@ -2,7 +2,8 @@
 
 command -v $dotnet >/dev/null 2>&1 || { echo >&2 "dotnet is not installed, aborting."; exit 1; }
 
-if [ "$(dotnet --version)" == *"3."* ]; then
+if [ "$(dotnet --version)" = *"3."* ]; then
+    echo "3.0"
     dotnet bin/netcoreapp3.0/Gauge.Dotnet.dll $@
 else
     dotnet bin/net5.0/Gauge.Dotnet.dll $@
