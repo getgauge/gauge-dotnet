ea39cc6cd5fdf6ef491d1cac69914bf43f771b7a
jeremyosterhoudt <jeremyosterhoudt@yahoo.com>
2022-01-15 03:15:05 -0800

    Add .Net 6 and ARM support (#174)
    * Added .Net 6 and ARM support
    
    Signed-off-by: Jeremy Osterhoudt <jeremyosterhoudt@yahoo.com>
    
    * bump actions/runtimes in build.yml
    
    Signed-off-by: sriv <srikanth.ddit@gmail.com>
    
    * bump version -> 0.5.0
    
    Signed-off-by: sriv <srikanth.ddit@gmail.com>
    
    * use temurin as java dist
    
    Signed-off-by: sriv <srikanth.ddit@gmail.com>
    
    * net3.0 is deprecated, use net3.1 instead as targetframework
    
    Signed-off-by: sriv <srikanth.ddit@gmail.com>
    
    * setup dotnet 6 explicitly in lsp tests
    
    Signed-off-by: sriv <srikanth.ddit@gmail.com>
    
    Co-authored-by: Jeremy Osterhoudt <jeremy.osterhoudt@ge.com>
    Co-authored-by: sriv <srikanth.ddit@gmail.com>

--- src/launcher.sh
+++ src/launcher.sh
@@ -3,6 +3,6 @@
 command -v $dotnet >/dev/null 2>&1 || { echo >&2 "dotnet is not installed, aborting."; exit 1; }
 
 case "$(dotnet --version)" in
-  3.*) dotnet bin/netcoreapp3.0/Gauge.Dotnet.dll $@ ;;
-  *) dotnet bin/net5.0/Gauge.Dotnet.dll $@ ;;
+  3.*) dotnet bin/netcoreapp3.1/Gauge.Dotnet.dll $@ ;;
+  *) dotnet bin/net6.0/Gauge.Dotnet.dll $@ ;;
 esac
