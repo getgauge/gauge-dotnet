7f49e22b5f749b5aa33703ca50706c6394fe9162
Piotr Nestorow <piotr.nestorow@systemverification.com>
2024-02-12 11:03:40 +0100

    Updated generating from proto files
    Signed-off-by: Piotr Nestorow <piotr.nestorow@systemverification.com>

---
+++ gen-proto.ps1
@@ -0,0 +1,32 @@
+# ----------------------------------------------------------------
+#   Copyright (c) ThoughtWorks, Inc.
+#   Licensed under the Apache License, Version 2.0
+#   See LICENSE.txt in the project root for license information.
+# ----------------------------------------------------------------
+
+dotnet restore
+
+$grpc_tools_version = "2.61.0"
+$grpc_tools = Join-Path $HOME ".nuget\packages\grpc.tools\$grpc_tools_version\tools"
+
+$protoc = $null
+$grpc_csharp = $null
+
+if ($env:PROCESSOR_ARCHITECTURE -match 64){
+    $protoc = Resolve-Path $grpc_tools\windows_x64\protoc.exe
+    $grpc_csharp = Resolve-Path $grpc_tools\windows_x64\grpc_csharp_plugin.exe
+}
+else {
+    $protoc = Resolve-Path $grpc_tools\windows_x86\protoc.exe
+    $grpc_csharp = Resolve-Path $grpc_tools\windows_x86\grpc_csharp_plugin.exe
+}
+
+Write-Host "Generating Proto Classes.."
+
+
+gci ".\gauge-proto" -Filter "*.proto" | %{
+    Write-Host "Generating classes for $_"
+    &$protoc @('-I.\gauge-proto', '--csharp_out=.\src\Gauge.CSharp.Core','--grpc_out=.\src\Gauge.CSharp.Core',"--plugin=protoc-gen-grpc=$grpc_csharp", ".\gauge-proto\$_")
+}
+
+Write-Host "Done!"
---
+++ genproto.sh
@@ -0,0 +1,69 @@
+#! /bin/bash
+
+set_os_architecture() {
+    echo "Detecting architecture"
+    local _ostype="$(uname -s)"
+    local _cputype="$(uname -m)"
+
+    echo "uname -s reports: $_ostype"
+    echo "uname -m reports: $_cputype"
+
+    if [ "$_ostype" = Darwin -a "$_cputype" = i386 ]; then
+        # Darwin `uname -s` lies
+        if sysctl hw.optional.x86_64 | grep -q ': 1'; then
+            local _cputype=x86_64
+        fi
+    fi
+
+    case "$_ostype" in
+
+        Linux)
+            local _ostype=linux
+            ;;
+        Darwin)
+            local _ostype=macosx
+            ;;
+        *)
+            #err "Unknown OS type: $_ostype"
+            ;;
+    esac
+
+    case "$_cputype" in
+
+        i386 | i486 | i686 | i786 | x86)
+            local _cputype=x86
+            ;;
+        x86_64 | x86-64 | x64 | amd64)
+            local _cputype=x64
+            ;;
+        *)
+            #err "Unknown CPU type: $_cputype"
+            ;;
+    esac
+
+    echo "OS is $_ostype"
+    echo "Architecture is $_cputype"
+    ARCH="$_cputype"
+    OS="$_ostype"
+}
+
+# mono .paket/paket.bootstrapper.exe
+# mono .paket/paket.exe install
+
+dotnet paket install
+
+set_os_architecture
+
+protoc=./packages/build/Grpc.Tools/tools/"$OS"_"$ARCH"/protoc.exe
+grpc_csharp=./packages/build/Grpc.Tools/tools/"$OS"_"$ARCH"/grpc_csharp_plugin.exe
+
+chmod +x $protoc
+chmod +x $grpc_csharp
+
+echo "Generating Proto Classes.."
+
+for i in `ls ./gauge-proto/*.proto`; do
+    $protoc -I./gauge-proto --csharp_out=./src/Gauge.CSharp.Core --grpc_out=./src/Gauge.CSharp.Core --plugin=protoc-gen-grpc=$grpc_csharp $i
+done
+
+echo "Done"
