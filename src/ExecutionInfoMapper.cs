6a98bc111b4e2f71112eb84ead388ccc9a840e09
Piotr Nestorow <piotr.nestorow@systemverification.com>
2024-06-18 15:39:13 +0200

    StepFrom parameters update
    Signed-off-by: Piotr Nestorow <piotr.nestorow@systemverification.com>

--- src/ExecutionInfoMapper.cs
+++ src/ExecutionInfoMapper.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using Gauge.Dotnet.Wrappers;
 using Gauge.Messages;
 using Gauge.CSharp.Lib;
+using Gauge.Dotnet.Processors;
 using System.Diagnostics.Tracing;
 using Microsoft.VisualBasic;
 
@@ -20,11 +21,13 @@ namespace Gauge.Dotnet
     {
         private Type _executionContextType;
         private readonly IActivatorWrapper activatorWrapper;
+        private readonly ITableFormatter tableFormatter;
 
         public ExecutionInfoMapper(IAssemblyLoader assemblyLoader, IActivatorWrapper activatorWrapper)
         {
             _executionContextType = assemblyLoader.GetLibType(LibType.ExecutionContext);
             this.activatorWrapper = activatorWrapper;
+            tableFormatter = new TableFormatter(assemblyLoader, activatorWrapper);
         }
 
         public dynamic ExecutionContextFrom(ExecutionInfo currentExecutionInfo)
@@ -66,32 +69,20 @@ namespace Gauge.Dotnet
                 return activatorWrapper.CreateInstance(executionContextStepType);
 
             var parameters = new List<List<string>>();
-            Table parametersTbl = null;
             foreach (var parameter in currentStep.Step.Parameters) {
-                if (parameter.ParameterType == Parameter.Types.ParameterType.Static)
+                if (parameter.ParameterType == Parameter.Types.ParameterType.Static) {
                     parameters.Add(new List<string> { "Static", parameter.Name, parameter.Value });
-
-                if (parameter.ParameterType == Parameter.Types.ParameterType.Dynamic)
+                }
+                else if (parameter.ParameterType == Parameter.Types.ParameterType.Dynamic) {
                     parameters.Add(new List<string> { "Dynamic", parameter.Name, parameter.Value });
-
-                if (parameter.ParameterType == Parameter.Types.ParameterType.SpecialString)
+                }
+                else if (parameter.ParameterType == Parameter.Types.ParameterType.SpecialString) {
                     parameters.Add(new List<string> { "Special", parameter.Name, parameter.Value });
-                    
-                if (parameter.ParameterType == Parameter.Types.ParameterType.SpecialTable ||
+                }
+                else if (parameter.ParameterType == Parameter.Types.ParameterType.SpecialTable ||
                     parameter.ParameterType == Parameter.Types.ParameterType.Table) {
-                    parameters.Add(new List<string> { "Table", parameter.Name, parameter.Value });
-
-                    List<string> headers = new List<string>();
-                    foreach (var header in parameter.Table.Headers.Cells)
-                        headers.Add(header);
-                    parametersTbl = new Table(headers);
-
-                    foreach (var currentRow in parameter.Table.Rows) {
-                        List<string> row = new List<string>();
-                        foreach (var item in currentRow.Cells)
-                            row.Add(item);
-                        parametersTbl.AddRow(row);
-                    }
+                    var asJSon = tableFormatter.GetJSON(parameter.Table);
+                    parameters.Add(new List<string> { "Table", parameter.Name, asJSon });
                 }
             }
 
@@ -99,7 +90,7 @@ namespace Gauge.Dotnet
                 executionContextStepType, 
                 currentStep.Step.ActualStepText, currentStep.IsFailed, 
                 currentStep.StackTrace, currentStep.ErrorMessage, 
-                parameters, parametersTbl);
+                parameters);
             
             return inst;
         }
