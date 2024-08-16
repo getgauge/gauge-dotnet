627e591ec211190e697d9311c0da84eb41c727ce
Chad Wilson <chadw@thoughtworks.com>
2024-08-16 21:56:24 +0800

    Bump grpc-tools version to match

--- genproto.sh
+++ genproto.sh
@@ -52,7 +52,7 @@ set_os_architecture
 
 dotnet restore
 
-grpc_tools_version="2.61.0"
+grpc_tools_version="2.65.0"
 protoc="$HOME"/.nuget/packages/build/grpc.tools/"grpc_tools_version"/tools/"$OS"_"$ARCH"/protoc
 grpc_csharp="$HOME"/.nuget/packages/build/grpc.tools/"grpc_tools_version"/tools/"$OS"_"$ARCH"/grpc_csharp_plugin
 
